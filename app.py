f99712ed3f48bab13f3cc9f29a2c52a2238b7b41
Kevin Perko <kperko@smartcoffeetech.com>
2013-07-25 16:32:32 -0700

    add gitignore file for pyc, hook up db, improve functions in app.py

--- app.py
+++ app.py
@@ -1,54 +1,121 @@
-from flask import Flask,jsonify,make_response,abort,render_template,request
+from flask import Flask,jsonify,make_response,abort,render_template,request,_app_ctx_stack
 import json
+from sqlite3 import dbapi2 as sqlite3
 
+#db_config
+DATABASE = '/tmp/floober.db'
+USERNAME = ''
+PASSWORD = ''
+
+#setup the app
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('APP_SETTINGS', silent=True)
+
+def connect_db():
+	top = _app_ctx_stack.top
+	if not hasattr(top,'sqlite_db'):
+		sqlite_db = sqlite3.connect(app.config['DATABASE'])
+		sqlite_db.row_factory = sqlite3.Row
+		top.sqlite_db = sqlite_db
+		
+	return top.sqlite_db
+	
+@app.teardown_appcontext
+def close_db_conn(exception):
+	top = _app_ctx_stack.top
+	if hasattr(top,'sqlite_db'):
+		top.sqlite_db.close()
+
 
 @app.route('/')
 @app.route('/api')
 def index():
+	db = connect_db()
+	cur = db.execute('Select driver_id from event_trip')
+	driver_list = cur.fetchall()
+	
+	drivers = []
+	
+	for each_driver in driver_list:
+		drivers.append({'driver_id':each_driver[0]})
+	
+	#return jsonify( {'driver': drivers})
 	return render_template('index.html')
 
 @app.errorhandler(404)
-def not_found(error):
+def resource_not_found(error):
 	return make_response(jsonify({'error':str('Resource not found: ' + request.url)}), 404)
 
 
 @app.route('/api/trips', methods= ['GET'])
 @app.route('/api/trips/<start_date>/<end_date>', methods = ['GET'])
 def get_number_of_trips(start_date=None,end_date=None):
-	""" sums the total number of trips"""
-	#cursor.execute("Select count(*) from event_trip")
-	#result = cursor.result()
-	#total_num_of_trips = result.fetch_one()
-		
+	""" sums the total number of trips format YYYY-MM-DD"""
+	
+	db = connect_db()
+	
+	if not start_date:
+		cur = db.execute('Select count(*) from event_trip')
+	elif start_date:
+		qry = "Select count(*) from event_trip where start_time between '%s' and '%s'" % (start_date,end_date)
+		cur = db.execute(qry)
+	
+	trip_count = cur.fetchall()
+	
+	trips_list = []
 	total_num_of_trips = {}
 	total_num_of_trips['start_date']=start_date
 	total_num_of_trips['end_date']=end_date
-	total_num_of_trips['trips']='33'
+	total_num_of_trips['trips']=trip_count[0][0]
+	trips_list.append(total_num_of_trips)
 
-	return jsonify({'total_number_of_trips' : total_num_of_trips})
+	return jsonify({'total_number_of_trips' : trips_list})
 	
 
 @app.route('/api/clients', methods = ['GET'])
 @app.route('/api/clients/<start_date>/<end_date>', methods = ['GET'])
 def get_total_number_of_clients(start_date=None,end_date=None):
-	""" sums the total number of trips"""
-	#cursor.execute("Select count(*) from event_trip")
-	#result = cursor.result()
-	#total_num_of_trips = result.fetch_one()
+	""" sums the total number of clients who've taken trips"""
+	
+	db = connect_db()
+	
+	if not start_date:
+		cur = db.execute('Select count(distinct(client_id)) from event_trip where driver_id is not null')
+	elif start_date:
+		qry = "Select count(distinct(client_id)) from event_trip where start_time between '%s' and '%s' and driver_id is not null" % (start_date,end_date)
+		cur = db.execute(qry)
+	
+	client_count = cur.fetchall()
+	
+	#added in these lists so I always return a list of dicts
+	clients_list = []
 	total_num_of_clients = {}
 	total_num_of_clients['start_date']=start_date
 	total_num_of_clients['end_date']=end_date
-	total_num_of_clients['clients']='33'
+	total_num_of_clients['clients']=client_count[0][0]
+	clients_list.append(total_num_of_clients)
 
 
-	return jsonify({'total_number_of_clients' : total_num_of_clients})
+	return jsonify({'total_number_of_clients' : clients_list})
 
 @app.route('/api/trips/hour', methods = ['GET'])
 def get_total_number_of_trips_last_hour():
-	pass
+	db = connect_db()
+	
 
-	return jsonify({'total_number_of_trips_last_hour' : total_num_of_trips_last_hour})
+	cur = db.execute("Select count(*) from event_trip where start_time between  datetime('now','-1 hours') and datetime('now') and driver_id is not null")
+	
+	trips_count = cur.fetchall()
+	
+	#added in these lists so I always return a list of dicts
+	trips_hr_list = []
+	total_num_of_trips = {}
+	total_num_of_trips['time_slice']='last_hour'
+	total_num_of_trips['clients']=client_count[0][0]
+	clients_hr_list.append(total_num_of_trips)
+
+	return jsonify({'total_number_of_trips_last_hour' : trips_hr_list})
 
 @app.route('/api/clients/miles', methods = ['GET'])
 @app.route('/api/clients/miles/<start_date>/<end_date>', methods = ['GET'])
--- models.sql
+++ models.sql
@@ -1,14 +1,26 @@
 -- CREATE TABLE STMT
 -- author: kevin p
+drop table if exists event_trip;
 create table event_trip(
-	client_id int unsigned not null,
-	driver_id int unsigned not null,
-	start_time TIMESTAMP default null,
-	lat decimal(16,9) default null,
-	lng decimal(16,9) default null,
-	fare float default null,
-	distance decimal(9,2) not null,
-	rating tinyint default null,
-	PRIMARY KEY(client_id), 
-	UNIQUE KEY(client,driver_id,start_time)
-)
+	row_id integer not null primary key autoincrement,
+	client_id integer not null,
+	driver_id integer not null,
+	start_time datetime not null,
+	lat float not null,
+	lon float not null,
+	fare float not null,
+	distance float not null,
+	rating integer not null, 
+	UNIQUE (client_id,driver_id,start_time)
+);
+
+INSERT INTO event_trip (client_id,driver_id,start_time,lat,lon,fare,distance,rating)
+ VALUES (1,2,'2013-07-01 11:33:33', 37.791812,-122.432759,12.39,3.54,5);
+INSERT INTO event_trip (client_id,driver_id,start_time,lat,lon,fare,distance,rating)
+ VALUES (1,3,'2013-06-01 11:33:33', 37.791812,-122.432759,22.39,5.54,1);
+INSERT INTO event_trip (client_id,driver_id,start_time,lat,lon,fare,distance,rating)
+ VALUES (2,2,'2013-07-02 11:33:33', 37.791812,-122.432759,2.39,1.54,1);
+INSERT INTO event_trip (client_id,driver_id,start_time,lat,lon,fare,distance,rating)
+ VALUES (3,4,'2013-07-25 11:31:33', 37.791812,-122.432759,32.39,6.54,2);
+INSERT INTO event_trip (client_id,driver_id,start_time,lat,lon,fare,distance,rating)
+ VALUES (2,5,'2013-07-25 11:33:33', 37.791812,-122.432759,16.39,4.54,3);
