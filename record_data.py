1d7def1fe7754edeb2111832fac6b7934c3c153f
Kevin Perko <kperko@smartcoffeetech.com>
2013-07-25 17:26:51 -0700

    add time pkg to generate unique timestamp on each loop

--- record_data.py
+++ record_data.py
@@ -2,33 +2,36 @@
 import sqlite3 as sql
 import logging
 import json
+import traceback
+import time
 
 logging.basicConfig(filename='db_exceptions.log', level=logging.DEBUG)
 
 #setup the database conn
 db = sql.connect('/tmp/floober.db')
 #instantiate the cursor class
-cursor = db.cursor()
+cur = db.cursor()
 
 
 def record_trip_event(client_id,driver_id,start_time,lat,lng,fare,distance,rating):
 	"""records trip event data to db - client_id,driver_id,start_time,lat,lng,fare,distance,rating"""
 	
-	sql_insert = """INSERT INTO event_trip (client_id,driver_id,start_time,lat,lng,fare,distance,rating) /
+	sql_insert = """INSERT INTO event_trip (client_id,driver_id,start_time,lat,lng,fare,distance,rating)
 	VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""" % (client_id,driver_id,start_time,lat,lng,fare,distance,rating)
 	
-	cursor.execute(sql_intert)
+	cur.execute(sql_insert)
 	db.commit()
+	print 'success'
 	
 
 def main():
 	#open a file
-	#incoming data is in json format
+	# is incoming data is in json format?
 	#process each line
 	
 	while True:
 		try:
-			record_trip_event(123,100,'07-21-31 12:13:15', '77.5093383','-34.39383839', '22.11', '4.3', '4')
+			record_trip_event(123,100,time.strftime('%Y-%m-%d %H:%M:%S'), '77.5093383','-34.39383839', '22.11', '4.3', '4')
 		except Exception, exc:
 			logging.debug(traceback.format_exc())
 	
